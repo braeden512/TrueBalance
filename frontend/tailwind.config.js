c90b51f66a6a87e7c6634c6abb235b7b44951c45
Braeden Treutel <braeden4328@gmail.com>
2025-09-04 23:54:26 -0500

    Changed to next.js, reinstalled recharts, installed shadcn/ui for prebuilt components, and cleaned up the directory

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      }
+    },
   },
-  plugins: [],
-}
-
+  plugins: [
+  ],
+};
